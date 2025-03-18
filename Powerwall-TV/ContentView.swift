4d7beb12d2d1410ab24fb6b37b83b6b33b517701
sighmon <simon.loffler@gmail.com>
2025-03-18 22:54:49 +1030

    Add home image and style the Powerwall values

--- Powerwall-TV/ContentView.swift
+++ Powerwall-TV/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftData
 
 struct ContentView: View {
     @StateObject private var viewModel: PowerwallViewModel
+    @State private var demo = false
     @State private var showingSettings = false
     private let timer = Timer.publish(every: 10, on: .main, in: .common).autoconnect()
 
@@ -21,51 +22,165 @@ struct ContentView: View {
     }
 
     var body: some View {
-        VStack(spacing: 20) {
-            if viewModel.ipAddress.isEmpty {
-                Text("Please configure the gateway settings.")
-                    .foregroundColor(.gray)
-            } else if let data = viewModel.data {
-                Text("⚡️ Grid: \(data.site.instantPower / 1000, specifier: "%.3f") kW")
-                Text("🔋 Powerwall: \(data.battery.instantPower / 1000, specifier: "%.3f") kW")
-                Text("🏡 Home: \(data.load.instantPower / 1000, specifier: "%.3f") kW")
-                Text("☀️ Solar: \(data.solar.instantPower / 1000, specifier: "%.3f") kW")
-                Text("💛 Total: \(data.solar.energyExported / 1000, specifier: "%.0f") kWh")
-            } else if let errorMessage = viewModel.errorMessage {
-                Text("Error: \(errorMessage)")
-                    .foregroundColor(.red)
-            } else {
-                Text("Loading...")
-            }
+        ZStack {
+            Image(uiImage: UIImage(named: "home.png")!)
+                .resizable()
+                .ignoresSafeArea()
+            ZStack {
+                if viewModel.ipAddress.isEmpty {
+                    Text("Please configure the gateway settings.")
+                        .foregroundColor(.gray)
+                } else if let data = viewModel.data {
+                    ZStack {
+                        VStack {
+                            HStack {
+                                VStack {
+                                    Text("\(data.solar.energyExported / 1000, specifier: "%.0f") kWh")
+                                        .fontWeight(.bold)
+                                        .font(.headline)
+                                    Text("ENERGY EXPORTED")
+                                        .opacity(0.6)
+                                        .fontWeight(.bold)
+                                        .font(.footnote)
+                                }
+                                Spacer()
+                            }
+                            Spacer()
+                        }
+                        VStack {
+                            HStack {
+                                Spacer().frame(width: 340)
+                                VStack {
+                                    Text("\(data.solar.instantPower / 1000, specifier: "%.3f") kW")
+                                        .fontWeight(.bold)
+                                        .font(.headline)
+                                    Text("SOLAR")
+                                        .opacity(0.6)
+                                        .fontWeight(.bold)
+                                        .font(.footnote)
+                                }
+                            }
+                            Spacer()
+                        }
+                        VStack {
+                            Spacer().frame(height: 100)
+                            HStack {
+                                Spacer().frame(width: 980)
+                                VStack {
+                                    Text("\(data.load.instantPower / 1000, specifier: "%.3f") kW")
+                                        .fontWeight(.bold)
+                                        .font(.headline)
+                                    Text("HOME")
+                                        .opacity(0.6)
+                                        .fontWeight(.bold)
+                                        .font(.footnote)
+                                }
+                            }
+                            Spacer()
+                        }
+                        VStack {
+                            Spacer()
+                            HStack {
+                                Spacer().frame(width: 120)
+                                VStack {
+                                    Text("\(data.battery.instantPower / 1000, specifier: "%.3f") kW")
+                                        .fontWeight(.bold)
+                                        .font(.headline)
+                                    Text("POWERWALL")
+                                        .opacity(0.6)
+                                        .fontWeight(.bold)
+                                        .font(.footnote)
+                                }
+                            }
+                        }
+                        VStack {
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                VStack {
+                                    Text("\(data.site.instantPower / 1000, specifier: "%.3f") kW")
+                                        .fontWeight(.bold)
+                                        .font(.headline)
+                                    Text("GRID")
+                                        .opacity(0.6)
+                                        .fontWeight(.bold)
+                                        .font(.footnote)
+                                }
+                            }
+                        }
+                    }
+                } else if let errorMessage = viewModel.errorMessage {
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Spacer().frame(width: 120)
+                            VStack {
+                                Text("Error: \(errorMessage)")
+                                    .opacity(0.6)
+                                    .fontWeight(.bold)
+                                    .font(.footnote)
+                            }
+                        }
+                    }
+                } else {
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Spacer().frame(width: 120)
+                            VStack {
+                                Text("Loading...")
+                                    .opacity(0.6)
+                                    .fontWeight(.bold)
+                                    .font(.footnote)
+                            }
+                        }
+                    }
+                }
 
-            Button(action: {
-                showingSettings = true
-            }) {
-                ZStack {
-                    Circle()
-                        .fill(Color.gray)
-                        .frame(width: 40, height: 40)
-                    Image(systemName: "gear")
-                        .font(.title)
+                VStack {
+                    Spacer()
+                    HStack {
+                        Button(action: {
+                            showingSettings = true
+                        }) {
+                            ZStack {
+                                Circle()
+                                    .fill(Color.gray)
+                                    .frame(width: 40, height: 40)
+                                Image(systemName: "gear")
+                                    .font(.title)
+                            }
+                        }
+                        .accessibilityLabel("Settings")
+                        Spacer()
+                    }
                 }
             }
-            .accessibilityLabel("Settings")
-            .padding(.top, 100)
-        }
-        .padding()
-        .sheet(isPresented: $showingSettings) {
-            SettingsView(ipAddress: $viewModel.ipAddress, username: $viewModel.username, password: $viewModel.password)
-        }
-        .onReceive(timer) { _ in
-            if !viewModel.ipAddress.isEmpty {
-                viewModel.fetchData()
+            .padding()
+            .sheet(isPresented: $showingSettings) {
+                SettingsView(ipAddress: $viewModel.ipAddress, username: $viewModel.username, password: $viewModel.password)
             }
-        }
-        .onAppear {
-            if viewModel.ipAddress.isEmpty {
-                showingSettings = true
-            } else {
-                viewModel.fetchData()
+            .onReceive(timer) { _ in
+                if !viewModel.ipAddress.isEmpty {
+                    viewModel.fetchData()
+                }
+            }
+            .onAppear {
+                if viewModel.ipAddress.isEmpty {
+                    showingSettings = true
+                } else if viewModel.ipAddress == "demo" {
+                    viewModel.data = PowerwallData(
+                        battery: PowerwallData.Battery(instantPower: 256),
+                        load: PowerwallData.Load(instantPower: 256),
+                        solar: PowerwallData.Solar(
+                            instantPower: 2048,
+                            energyExported: 4096000
+                        ),
+                        site: PowerwallData.Site(instantPower: 1024)
+                    )
+                } else {
+                    viewModel.fetchData()
+                }
             }
         }
     }
